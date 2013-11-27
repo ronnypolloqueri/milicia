d8b1c7e3b308a31e397ceda0eacc5e846d8fa653
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2013-11-27 14:01:32 -0500

    Agregada navegacion atras siguiente a RegionMilitar, y extendida para todos los objetos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+	def anterior(objeto)
+		if(objeto.id == 1)
+			return (objeto.class).find(numero_de_registros(objeto))
+		else
+			return (objeto.class).find(objeto.id - 1)
+		end
+	end
+
+	def siguiente(objeto)
+		if(objeto.id == numero_de_registros(objeto))
+			return (objeto.class).find(1)
+		else
+			return (objeto.class).find(objeto.id + 1)
+		end
+	end
+
+	def numero_de_registros(objeto)
+		(objeto.class).count
+	end
 end
--- app/helpers/gran_unidads_helper.rb
+++ app/helpers/gran_unidads_helper.rb
@@ -1,23 +1,23 @@
 module GranUnidadsHelper
 	#Para la vista show
 	#Los id deben ser consecutivos
-	def anterior(id)
-		if(id == 1)
-			return GranUnidad.find(numero_de_registros)
-		else
-			return GranUnidad.find(id - 1)
-		end
-	end
+	# def anterior(id)
+	# 	if(id == 1)
+	# 		return GranUnidad.find(numero_de_registros)
+	# 	else
+	# 		return GranUnidad.find(id - 1)
+	# 	end
+	# end
 
-	def siguiente(id)
-		if(id == numero_de_registros)
-			return GranUnidad.find(1)
-		else
-			return GranUnidad.find(id + 1)
-		end
-	end
+	# def siguiente(id)
+	# 	if(id == numero_de_registros)
+	# 		return GranUnidad.find(1)
+	# 	else
+	# 		return GranUnidad.find(id + 1)
+	# 	end
+	# end
 
-	def numero_de_registros
-		GranUnidad.count
-	end
+	# def numero_de_registros
+	# 	GranUnidad.count
+	# end
 end
