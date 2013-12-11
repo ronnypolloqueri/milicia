c6a57192d1ad89044b715a7f721282d24c5914a9
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2013-12-11 11:01:49 -0500

    Solucionado problema de aproximadamente 3 o 4 migraciones  en donde se agregaba la referencia grado a personal 20131211134540_agrega_cambios_a_personal cuando esta ya estaba agregada en una migracion 20131126202143_add_grado_to_personal

--- db/migrate/20131211134540_agrega_cambios_a_personal.rb
+++ db/migrate/20131211134540_agrega_cambios_a_personal.rb
@@ -1,6 +1,5 @@
 class AgregaCambiosAPersonal < ActiveRecord::Migration
   def change
-  	add_reference :personal, :grado, index: true
   	add_column :personal, :fecha_inscripcion, :date
   	add_column :personal, :fecha_licenciado, :date
   end
